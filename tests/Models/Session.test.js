443f92d49de4b652b11b66071e6b5108f423a869
Dave Irvine <dave@daveirvine.me>
2015-02-13 21:13:18 +0000

    Compiler commit

--- tests/Models/Session.test.js
+++ tests/Models/Session.test.js
@@ -76,7 +76,7 @@
         });
       });
     });
-    return describe("logout", function() {
+    describe("logout", function() {
       var requestStub, session;
       session = void 0;
       requestStub = void 0;
@@ -112,7 +112,7 @@
         requestStub.restore();
         requestStub = sinon.stub(apiClient, "request", function(method) {
           return new Promise(function(resolve, reject) {
-            if (method !== "logout") {
+            if (method !== "session.logout") {
               return resolve(sessionRef);
             } else {
               return resolve();
@@ -133,6 +133,106 @@
           var promise;
           promise = session.logout();
           return expect(promise).to.eventually.be.fulfilled.and.notify(done);
+        })["catch"](function(e) {
+          return done(e);
+        });
+      });
+    });
+    return describe("request", function() {
+      var requestStub, session;
+      session = void 0;
+      requestStub = void 0;
+      beforeEach(function() {
+        requestStub = sinon.stub(apiClient, "request", function() {
+          return new Promise(function(resolve, reject) {
+            return resolve();
+          });
+        });
+        return session = new Session(apiClient);
+      });
+      afterEach(function() {
+        return requestStub.restore();
+      });
+      it("should throw if not logged in", function() {
+        return expect(function() {
+          return session.request();
+        }).to["throw"](/Must be logged in to make API requests/);
+      });
+      it("should call `request` on apiClient if logged in", function(done) {
+        return session.login().then(function() {
+          session.request();
+          expect(requestStub).to.have.been.called;
+          return done();
+        })["catch"](function(e) {
+          return done(e);
+        });
+      });
+      it("should call `request` with the method name passed to it", function(done) {
+        return session.login().then(function() {
+          session.request("test.method");
+          expect(requestStub).to.have.been.calledWith("test.method");
+          return done();
+        })["catch"](function(e) {
+          return done(e);
+        });
+      });
+      it("should call `request` with the args passed to it and sessionID", function(done) {
+        var sessionRef;
+        sessionRef = "abcd1234";
+        requestStub.restore();
+        requestStub = sinon.stub(apiClient, "request", function(method) {
+          return new Promise(function(resolve, reject) {
+            return resolve(sessionRef);
+          });
+        });
+        return session.login().then(function() {
+          session.request("test.method");
+          expect(requestStub).to.have.been.calledWith("test.method", [sessionRef]);
+          return done();
+        })["catch"](function(e) {
+          return done(e);
+        });
+      });
+      it("should resolve with the value resolved by apiClient if successful", function(done) {
+        var resolveValue;
+        resolveValue = "test123";
+        requestStub.restore();
+        requestStub = sinon.stub(apiClient, "request", function(method) {
+          return new Promise(function(resolve, reject) {
+            if (method !== "test.method") {
+              return resolve();
+            } else {
+              return resolve(resolveValue);
+            }
+          });
+        });
+        return session.login().then(function() {
+          return session.request("test.method").then(function(value) {
+            expect(value).to.equal(resolveValue);
+            return done();
+          });
+        })["catch"](function(e) {
+          return done(e);
+        });
+      });
+      return it("should reject with the error rejected by apiClient if failure", function(done) {
+        var rejectValue;
+        rejectValue = "test123";
+        requestStub.restore();
+        requestStub = sinon.stub(apiClient, "request", function(method) {
+          return new Promise(function(resolve, reject) {
+            if (method !== "test.method") {
+              return resolve();
+            } else {
+              return reject(rejectValue);
+            }
+          });
+        });
+        return session.login().then(function() {
+          return session.request("test.method")["catch"](function(e) {
+            expect(e).to.equal(rejectValue);
+            return done();
+          });
         })["catch"](function(e) {
           return done(e);
         });
